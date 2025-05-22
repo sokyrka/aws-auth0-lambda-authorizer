c9b65995b07952b1b8fe032a0bcbdefbccf01e24
Ievgenii Sokyrka <ICIevgenii.Sokyrka@amwell.com>
2025-05-22 11:23:14 +0200

    Added env variables for local testing

--- authorizerHandler.ts
+++ authorizerHandler.ts
@@ -1,25 +1,27 @@
-import { APIGatewayAuthorizerResult, Context } from 'aws-lambda';
+import {APIGatewayAuthorizerResult} from 'aws-lambda';
 
-export const handler = async (event: any, context: Context) => {
-    const authHeader = event.headers.authorization;
+export const handler = async (event: any) => {
+    const authHeader = event.authorizationToken;
 
     if (!authHeader) {
         return denyAllPolicy();
     }
 }
 
+
+/**
+ * Return an IAM policy that denies access to the API
+ */
 function denyAllPolicy(): APIGatewayAuthorizerResult {
     return {
-        "principalId": "*",
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Action": "*",
-                    "Effect": "Deny",
-                    "Resource": "*"
-                }
-            ]
+        principalId: '*',
+        policyDocument: {
+            Version: '2012-10-17',
+            Statement: [{
+                Action: 'execute-api:Invoke',
+                Effect: 'Allow',
+                Resource: '*',
+            }]
         }
-    }
+    };
 }
